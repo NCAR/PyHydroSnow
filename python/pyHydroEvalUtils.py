ad25e23c7cb178de66de808674d4bbb8b4b27abe
Logan Karsten <karsten@pequin.rap.ucar.edu>
2016-08-26 15:04:09 -0600

    test

--- python/pyHydroEvalUtils.py
+++ python/pyHydroEvalUtils.py
@@ -97,11 +97,11 @@ def initNamelist(args,dbIn,rank,size):
     print str(rank)
     print str(args.begADate) + ".R"
     # Establish parent directory path where original namelist file will live.
-    nameListFileOrig = "namelist_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(args.begADate) + ".R" #+ \
-                       #str(args.endADate) + "_" = strTmp.join(args.modelProjects) + "_" + \
-                       #str(args.snRead) + "_" + str(args.stRead) + "_" + str(args.snRun) + "_" + \
-                       #str(args.stRun) + "_subset_" + str(args.subset) + "_pad_" + str(args.pad) + \
-                       #"_RANK_" + str(rank) + ".R"
+    nameListFileOrig = "namelist_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(args.begADate) + ".R" + \
+                       str(args.endADate) + "_" + strTmp.join(args.modelProjects) + "_" + \
+                       str(args.snRead) + "_" + str(args.stRead) + "_" + str(args.snRun) + "_" + \
+                       str(args.stRun) + "_subset_" + str(args.subset) + "_pad_" + str(args.pad) + \
+                       "_RANK_" + str(rank) + ".R"
     print nameListFileOrig
     nameListPathOrig = dbIn.topDir[indDbOrig] + "/" + dbIn.alias[indDbOrig] + "/namelists/" + nameListFileOrig
 
