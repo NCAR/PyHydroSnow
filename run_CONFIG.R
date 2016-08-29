a2237184c2c7aa7b53d0a22d03ffe7b95ca5119e
Logan Karsten <karsten@pequin.rap.ucar.edu>
2016-08-29 11:53:33 -0600

    Update 8/29/2016

--- run_CONFIG.R
+++ run_CONFIG.R
@@ -160,7 +160,7 @@ if (size > 1){
         mskhyd.nameList <- listMpi[15]
         mskgeo.nameList <- listMpi[16]  
         # Reset gageList
-        print(mskgeo.List)
+        print(stid2gageList)
         gageList <- data.frame(st_id=names(stid2gageList), site_no=unlist(stid2gageList), stringsAsFactors=FALSE)  
         print(mskgeo.nameList)
     }
