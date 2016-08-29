6c1ee7b5c167749059176d65463936d768408fd6
Logan Karsten <karsten@pequin.rap.ucar.edu>
2016-08-29 10:12:20 -0600

    Update 8/29/2016

--- util_FUNC.R
+++ util_FUNC.R
@@ -173,7 +173,7 @@ subSetBasins <- function(mskgeo.nameList,
                          stid2gageList,
                          subSet){
 
-    subSet <- subSet
+    print(subSet)
     # First determine which subset names are of type forecast points.
     fxstPtsSub = which(unique(subSet$type) == 2)
     ind <- c()
