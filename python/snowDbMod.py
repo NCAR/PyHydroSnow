f1c6b4e777a68a529c9dcb89e2e2cb247a454093
Logan Karsten <karsten@pequin.rap.ucar.edu>
2016-11-17 17:16:23 -0700

    Update 2016-11-17

--- python/snowDbMod.py
+++ python/snowDbMod.py
@@ -13,7 +13,7 @@ import datetime
 import subprocess
 
 def extractObs(begRDateObj,endRDateObj,outDir,geoFile,networkFile='',stnFile='',maskFile=''):
-    print "MASK FILE = " + maskFile + " YAAAASSS"
+    print "MASK FILE = " + networkFile + " YAAAASSS"
     # Establish paths
     fileOut = outDir + "/" + "SNOW_DB_OBS_" + begRDateObj.strftime('%Y%m%d%H') + \
               "_" + endRDateObj.strftime('%Y%m%d%H') + '.nc'
